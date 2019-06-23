fe914a00a9bedf0d96b9c96a7e654659b8d441aa
KatherineDiaz <kdiaz201294@gmail.com>
2019-06-23 17:40:15 -0300

    Alta de Usuario nuevo

--- LC/Usuario.cs
+++ LC/Usuario.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
 namespace LC
 {
     public class Usuario
@@ -13,6 +15,50 @@ namespace LC
         public string Contraseña { get; set; }
         public int IDPregunta { get; set; }
         public string Respuesta{ get; set; }
-        
+
+        public Pregunta pregunta;
+
+        public bool UsuarioAccion(Usuario usuario, string ejecutar)
+        {
+            var conn = new SqlConnection();
+            var comando = new SqlCommand();
+            var baseDatos = new Connect();
+            bool rta;
+            try
+            {
+                conn = baseDatos.Abrir();
+                comando.Connection = conn;
+
+                if (ejecutar == "ALTA")
+                {
+                    comando.CommandText = "Insert into Cuenta (nickname, email, contraseña, FK_ID_Pregunta, Respuesta ) values ('" + usuario.Nick + "','" + usuario.Mail + "','" + usuario.Contraseña + "','" + usuario.IDPregunta + "','" + usuario.Respuesta  + "')";
+                }
+            
+                else if (ejecutar == "EDITAR")
+                {
+                    comando.CommandText = "UPDATE Cuenta SET contraseña=" + usuario.Contraseña+ " WHERE nickname=" + usuario.Nick;
+                }
+                
+                comando.ExecuteNonQuery();
+                rta = true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Concat(e.Message, e.StackTrace), "");
+                rta = false;
+            }
+            finally
+            {
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+            }
+            return rta;
+
+        }
+
     }
 }
