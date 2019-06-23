fe53721329943aef753795aac7813f6d5352ec26
Alanjavier11 <alan-capo99@hotmail.com>
2019-06-23 15:43:41 -0300

    ACTUALIZAR CLIENTE EN EL DISEÑO DEL MENU

--- CU/Login.Designer.cs
+++ CU/Login.Designer.cs
@@ -127,10 +127,10 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(76, 316);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(113, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(119, 13);
             this.linkLabel1.TabIndex = 15;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Olvido su contraseña?";
+            this.linkLabel1.Text = "¿Olvido su contraseña?";
             // 
             // button2
             // 
